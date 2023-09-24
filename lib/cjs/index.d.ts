ddfb00c3d94d2828a739deb1da49fe6492688a37
Jonathan Cobb <jonathan@kyuss.org>
2023-09-24 11:50:58 -0400

    remove cjs, only export esm

--- lib/cjs/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "mobiletto-common";
-export * from "./crypt.js";
-export * from "./types.js";
-export * from "./mobiletto.js";
-export { registerDriver, shutdownMobiletto } from "./register.js";
-export { flushAll } from "./redis.js";
-export { MobilettoEncryptionSettings, encrypt, decrypt } from "./crypt.js";
